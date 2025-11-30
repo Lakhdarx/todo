d326e720de55f5997cae746787e12dd9038604cd
Lakhdar <lakhdarbhl76@gmail.com>
2025-11-30 12:31:17 +0100

    add todo edit feature with form pre-filled

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,12 +1,12 @@
 import { renderProjects, renderTodos } from "./dom";
 import { projectManager } from "./projectManager";
-// import { Todo } from "./todo";
 
 export { initUI };
 
 function setupAddTodoBtn() {
   const addBtn = document.querySelector("#add-todo");
   const modal = document.querySelector(".modal");
+  const form = document.querySelector("#add-todo-form");
 
   addBtn.addEventListener("click", () => {
     modal.classList.add("show");
@@ -15,6 +15,7 @@ function setupAddTodoBtn() {
   modal.addEventListener("click", (e) => {
     if (e.target === modal) {
       modal.classList.remove("show");
+      form.reset();
     }
   });
 }
@@ -60,18 +61,69 @@ function setupTodoForm() {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    const editingId = document.querySelector("#editing-id").value;
+
     const title = document.querySelector("#title").value;
     const description = document.querySelector("#description").value;
     const dueDate = document.querySelector("#due-date").value;
     const priority = document.querySelector("#priority").value;
     const notes = document.querySelector("#notes").value;
 
-    projectManager
-      .getActiveProject()
-      .addTodo(title, description, dueDate, priority, notes);
+    const currentProj = projectManager.getActiveProject();
+
+    if (editingId) currentProj.removeTodo(editingId);
+
+    currentProj.addTodo(title, description, dueDate, priority, notes);
 
     renderTodos(projectManager.getActiveProject().todos);
     modal.classList.remove("show");
+    form.reset();
+  });
+}
+
+function setupEditTodo() {
+  const todos = document.querySelector(".todos");
+  const form = document.querySelector("#add-todo-form");
+  const modal = document.querySelector(".modal");
+
+  todos.addEventListener("click", (e) => {
+    const item = e.target.closest(".todo-item");
+    if (
+      e.target.matches("input[type='checkbox']") ||
+      e.target.classList.contains("todo-delete") ||
+      !item
+    )
+      return;
+
+    const id = item.querySelector("input").dataset.id;
+    const currentProj = projectManager.getActiveProject();
+
+    modal.classList.add("show");
+
+    // Fill up the form with current todo values.
+    form.querySelector("#title").value = currentProj.todos.find(
+      (t) => t.id === id
+    ).title;
+
+    form.querySelector("#description").value = currentProj.todos.find(
+      (t) => t.id === id
+    ).description;
+
+    form.querySelector("#due-date").value = currentProj.todos.find(
+      (t) => t.id === id
+    ).dueDate;
+
+    form.querySelector("#priority").value = currentProj.todos.find(
+      (t) => t.id === id
+    ).priority;
+
+    form.querySelector("#notes").value = currentProj.todos.find(
+      (t) => t.id === id
+    ).notes;
+
+    //
+
+    document.querySelector("#editing-id").value = id;
   });
 }
 
@@ -119,4 +171,5 @@ function initUI() {
   setupRemoveTodo();
   switchProject();
   setupCheckboxes();
+  setupEditTodo();
 }
